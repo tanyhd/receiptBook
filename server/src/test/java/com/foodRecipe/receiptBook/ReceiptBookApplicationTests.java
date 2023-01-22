85743447bdff0a7b0b8464ebae5c811038357d00
Darren <darren243@hotmail.com>
2023-01-22 12:04:15 +0800

    Initial commit

---
+++ server/src/test/java/com/foodRecipe/receiptBook/ReceiptBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodRecipe.receiptBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
