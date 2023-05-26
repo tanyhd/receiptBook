8859b97333814c55a7c6681ac2295bf786cd2e9a
Darren <darren243@hotmail.com>
2023-05-26 11:10:05 +0800

    add list of endpoints to spring security to allow swagger ui

--- server/src/main/java/com/foodRecipe/recipeBook/config/SecurityConfiguration.java
+++ server/src/main/java/com/foodRecipe/recipeBook/config/SecurityConfiguration.java
@@ -26,7 +26,21 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**", "/api/v1/demo/**", "/api/recipes/**", "/**")
+                .requestMatchers(
+                        "/api/v1/auth/**",
+                        "/api/v1/demo/**",
+                        "/api/recipes/**",
+                        "/v2/api-docs",
+                        "/v3/api-docs",
+                        "/v3/api-docs/**",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "configuration/ui",
+                        "configuration/security",
+                        "/swagger-ui/**",
+                        "/webjars/**",
+                        "/swagger-ui.html"
+                )
                 .permitAll()
                 .anyRequest()
                 .authenticated()
